b225b9183b7360be692ed48bf33455efccbc2964
Mark Wilson <markwilson107@hotmail.com>
2020-10-17 17:37:49 +0800

    Added mobile lock and fixed login

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,20 +1,8 @@
 //load placeholder images
 $(document).ready(function () {
-    // preview navbar
-    var $pnav = $("<nav>");
-    $pnav.addClass("navbar navbar-expand-lg navbar-light border-bottom border-left border-right preview-nav")
-    var $navBrand = $("<a>");
-    $navBrand.addClass("navbar-brand preview-title");
-    $navBrand.text("Preview");
-    $pnav.append($navBrand);
-
-    $(".grid-container").append($pnav);
     // create temp grid  
     var $cont = $("<div>");
     $cont.addClass("container-fluid")
-    //var $navbtn = $("<button>");
-    //$navbtn.text("button");
-    //$pnav.append($navbtn);
     var $row = $("<div>");
     $row.addClass("row grid-row-drag")
     for (var i = 0; i < 24; i++) {
@@ -22,49 +10,51 @@ $(document).ready(function () {
         $row.append(`<div class="col-4 img-grid-box"><div class="img-grid" style="background-image:url('./assets/img/placeholder.png');"></div></div>`);
     }
     $cont.append($row);
-    $(".grid-container").append($cont);
-
+    $(".grid-preview").append($cont);
 
     // --- Story Board ---
     // create temp grid  
     var $scont = $("<div>");
     $scont.addClass("container-fluid")
-    //var $navbtn = $("<button>");
-    //$navbtn.text("button");
-    //$pnav.append($navbtn);
     var $srow = $("<div>");
     $srow.addClass("row grid-row-drag")
     for (var i = 0; i < 24; i++) {
-
         $srow.append(`<div class="col-4 img-story-box"><div class="img-story" style="background-image:url('./assets/img/placeholder.png');"></div></div>`);
     }
     $scont.append($srow);
-    $storyCard = $(`<div>`);
-    $storyCard.addClass(`card`);
-    $storyCardBody = $(`<div>`);
-    $storyCardBody.addClass(`card-body`);
-    $storyCardBody.append($scont);
-    $storyCard.append($storyCardBody);
-    $(".story-container").html("<h3 class='story-title'>Storyboard</h3>")
-    $(".story-container").append($storyCard);
+    $(".story-preview").append($scont);
     //$(".grid-srow-drag").dad({
     //    exchangeable: true,
     //    placeholderTemplate: "<div style=\"border: 1px solid rgb(160, 160, 160 );\"></div>"
     //});
 
-
 })
 
-$(document).click( function (event) {
+$(document).click(function (event) {
     if (event.target.className === "img-grid") {
-      console.log("img-grids")
+        console.log("img-grids")
     }
 
 })
-
-$(".login").on("click", function () {
-    loadUser("tina.cohen")
+// Check if user pressed enter in login
+$(".login-username").on("keyup", function (e) {
+    if (e.key == "Enter" && e.target.value !== "") {
+        $('#loginModal').modal("hide")
+        loadUser(e.target.value);
+    }
 })
+// checks if user presses submit on login
+$(".login-submit").on("click", function () {
+    if ($(".login-username").val() !== "") {
+        loadUser($(".login-username").val());
+    }
+});
+
+//
+
+
+
+
 
 $(".save").on("click", function () {
     localStorage.setItem("userLayout", $(".grid-container")[0].innerHTML);
@@ -86,24 +76,11 @@ function loadUser(username) {
         success: function (data) {
             console.log(data);
             // clear grid
-            $(".grid-container").html("");
-            
-            // create temp grid
-            // preview navbar
-            var $pnav = $("<nav>");
-            $pnav.addClass("navbar navbar-expand-lg navbar-light border-bottom border-left border-right preview-nav")
-            var $navBrand = $("<a>");
-            $navBrand.addClass("navbar-brand preview-title");
-            $navBrand.text("Preview");
-            $pnav.append($navBrand);
+            $(".grid-preview").html("");
 
-            $(".grid-container").append($pnav);
             // create temp grid  
             var $cont = $("<div>");
             $cont.addClass("container-fluid")
-            //var $navbtn = $("<button>");
-            //$navbtn.text("button");
-            //$pnav.append($navbtn);
             var $row = $("<div>");
             $row.addClass("row grid-row-drag")
             for (var i = 0; i < data.graphql.user.edge_owner_to_timeline_media.edges.length; i++) {
@@ -111,8 +88,21 @@ function loadUser(username) {
                 $row.append(`<div class="col-4 img-grid-box no-drag item"><div class="img-grid" style="background-image:url('${data.graphql.user.edge_owner_to_timeline_media.edges[i].node.display_url}');"></div></div>`);
             }
             $cont.append($row);
-            $(".grid-container").append($cont);
-            
+            $(".grid-preview").append($cont);
+
+            // --- Story Board ---
+            // create temp grid  
+            $(".story-preview").html("");
+            var $scont = $("<div>");
+            $scont.addClass("container-fluid")
+            var $srow = $("<div>");
+            $srow.addClass("row grid-row-drag")
+            for (var i = 0; i < 24; i++) {
+                $srow.append(`<div class="col-4 img-story-box"><div class="img-story" style="background-image:url('./assets/img/placeholder.png');"></div></div>`);
+            }
+            $scont.append($srow);
+            $(".story-preview").append($scont);
+
             //user info
             $(".user-name").html(data.graphql.user.username);
             $(".user-fullname").html(data.graphql.user.full_name);
@@ -120,10 +110,26 @@ function loadUser(username) {
             $(".user-avatar").attr("src", data.graphql.user.profile_pic_url)
             $(".user-followers").html(data.graphql.user.edge_followed_by.count.toLocaleString());
             $(".user-following").html(data.graphql.user.edge_follow.count.toLocaleString());
-            
-            $(".grid-row-drag").dad({
+
+            var mainDAD = $(".grid-row-drag").dad({
+                active: true,
                 placeholderTemplate: "<div style=\"border: 1px solid rgb(160, 160, 160);opacity:30%;\"></div>"
             });
+
+            $("#myBtn").on("click", function () {
+                if ($("#myBtn").attr("toggle") === "OFF") {
+                    mainDAD.activate();
+                    $("#myBtn").attr("toggle", "ON");
+                    $(".grid-btn-img").attr("src", "./assets/img/th-solid.png");
+
+                    console.log("on");
+                } else if ($("#myBtn").attr("toggle") === "ON") {
+                    mainDAD.deactivate();
+                    $("#myBtn").attr("toggle", "OFF");
+                     $(".grid-btn-img").attr("src", "./assets/img/th-lock.png");
+                    console.log("off");
+                }
+            })
 
         },
         error: function (e) {
